4f911625e19a373ca19421b09e64db2fe3ec91c9
marcelo-rborges <marcelo.rborges@hotmail.com.br>
2021-09-10 12:56:50 -0300

    rewhite

--- src/app/config/origens/config-produtos.ts
+++ src/app/config/origens/config-produtos.ts
@@ -6,7 +6,7 @@ export const CONFIG_PRODUTOS = {
   // },
   /* Tipo de origem */
   // Se '' ignora essa origem de dados (não sincroniza).
-  tipo: 'db', // 'fb', 'db' | 'csv' | ''
+  tipo: '', // 'fb', 'db' | 'csv' | ''
 
   // Nome da view do cadastro de produtos de promoções
   // nomeView: 'View_IntegracaoMercadeiro2', // db/fb
